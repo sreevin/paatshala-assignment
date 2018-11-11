a5096be7795eea1156c8dcaaf2d11919d236acb7
sreevin <sreevinak1@gmail.com>
2018-11-11 18:46:40 +0530

    paatshala Assignment1

---
+++ script.js
@@ -0,0 +1,16 @@
+function read()
+{
+    var name = document.getElementById('name').value;
+    var dob = document.getElementById('dob').value;
+    var email = document.getElementById('email').value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var confirmpassword = document.getElementById('confirmpassword').value;
+
+    console.log(name);
+    console.log(dob);
+    console.log(email);
+    console.log(username);
+    console.log(password);
+    console.log(confirmpassword);
+}
